c9967dfe75fa444b247bf761494a4837cf894e22
Ordinateur 1 <abcvika3@gmail.com>
2026-01-09 14:55:36 +0100

    add lesson1

---
+++ module-01/lesson-01/script.js
@@ -0,0 +1,98 @@
+const myName = "Viktoria";
+console.log(myName);
+
+const birthMonth = 6;
+console.log(birthMonth);
+
+
+const str = " Ласкаво " + "просимо " + "на курс";
+console.log(str);
+
+// alert(2019);
+
+const operation = 2019 - 200;
+console.log(operation);
+
+//5
+
+const containerEl = document.getElementById("six");
+console.log(containerEl);
+containerEl.textContent = "Hello World";
+
+//6
+const wrapperEl = document.getElementByld("seven");
+console.log(wrapperEl);
+wrapperEl.textContent = 12 * 12;
+
+//7
+const out8 = document.querySelector(".out-8");
+console.log(out8);
+out8.textContent = "task-8";
+
+const out9 = document.querySelector(".out-8");
+console.log(out9);
+
+const elementOut10 = document.querySelector(out10);
+console.log(data[0] elementOut10, data[1]elementOut10);
+elementOut10.innerHTML = "<h2>Hi</h2>";
+// elementOut10.textContent = "<h2>Hi</h2>";
+
+//11
+
+const elementOut11 = document.querySelector(".out-11");
+console.log(data[0] elementOut11, data[1]elementOut11);
+elementOut11.innerHTML = "123";
+elementOut11.innerHTML += "456";
+
+//12
+const a = document.querySelector(.out-12);
+console.log( data0 a , data1 a);
+a.innerHTML = 3.1415;
+// a.innerHTML = Math.ceil(3.1415);
+
+const out-13 = documentSelector(".out-13");
+console.log(data 0 out13, data1 out13);
+out13.innerHTML = "https://image.unsplash.com/photo-1579468118864   alt-js width"
+
+//14
+
+const z1 = 6;
+const z2 = 3;
+const out14 = document.querySelector(".out14");
+const multiplication = z1 * z2;
+console.log(multiplication);
+out14.innerHTML = multiplication;
+
+//15
+const y1 = 6;
+const y2 = 3;
+const out15 = document.querySelector("out15");
+const divition = y1 / y2;
+console.log(divition);
+out15.innerHTML = divition;
+
+//16
+const x1 = "Hello";
+const x2 = 5;
+const out16 = document.querySelector(".out16");
+const summ = x1 + x2;
+console.log(summ);
+out16.innerHTML = summ;
+
+//17
+const out17 = document.querySelector(".out17");
+console.dir(out17);
+
+//18
+let out18 = document.querySelector("out18");
+out18 = 5;
+console.log(out18);
+
+//19
+let out19 = document.querySelector(".out19");
+console.log(out19);
+out19 = document.querySelector(".out19-test");
+console.log(out19);
+
+//20
+let out20 = 

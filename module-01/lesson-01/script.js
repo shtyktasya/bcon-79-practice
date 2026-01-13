88497da1e702f32f8f26321d33e73ba7c5bb87f2
shtyktasya <stiktasa@gmail.com>
2026-01-14 01:08:31 +0200

    up

---
+++ module-01/lesson-01/script.js
@@ -0,0 +1,106 @@
+//Task 1//
+const myName = "Таїсія";
+console.log(myName);
+//Task 2//
+const monthBirthday = 1;
+console.log(monthBirthday);
+//task 3 //
+const str = "Ласкаво " + " просимо " + " на курс";
+console.log("🚀 ~ str:", str);
+
+//Task 4//
+//alert(2019);//
+
+// Task 5//
+console.log(2019 - 200);
+
+//Task 6//
+const containerEl = document.getElementById("six");
+console.log("🚀 ~ containerEl:", containerEl);
+containerEl.textContent = "Hello World";
+
+//Task 7//
+const boxEl = document.getElementById("seven");
+console.log("🚀 ~ boxEl:", boxEl);
+boxEl.textContent = 12 * 12;
+
+//Task 8//
+const wrapperEl = document.querySelector(".out-8");
+console.log("🚀 ~ wrapperEl:", wrapperEl);
+wrapperEl.textContent = "task-8";
+
+//Task 9//
+const out9 = document.querySelector(".out-9");
+console.log("🚀 ~ out9:", out9);
+out9.textContent = "World";
+
+//Task 10//
+const out10 = document.querySelector(".out-10");
+console.log("🚀 ~ out10:", out10);
+out10.innerHTML = "<h2>Hi</h2>";
+
+//Task 11//
+const out11 = document.querySelector(".out-11");
+console.log("🚀 ~ out11:", out11);
+out11.innerHTML = "123";
+out11.innerHTML += "453";
+
+//Task 12//
+const a = document.querySelector(".out-12");
+console.log("🚀 ~ a:", a);
+a.innerHTML = 3.1415;
+
+//Task 13//
+const out13 = document.querySelector(".out-13");
+console.log("🚀 ~ out13:", out13);
+out13.innerHTML =
+  '<img src="https://images.unsplash.com/photo-1579468118864-1b9ea3c0db4a" alt="js" width="400" >';
+
+//Task 14//
+const out14 = document.querySelector(".out-14");
+const z1 = 6;
+const z2 = 3;
+out14.textContent = z1 * z2;
+console.log("🚀 ~ out14:", out14);
+console.log("🚀 ~ z1:", z1);
+console.log("🚀 ~ z2:", z2);
+
+//Task 15//
+const out15 = document.querySelector(".out-15");
+console.log("🚀 ~ out15:", out15);
+const y1 = 6;
+console.log("🚀 ~ y1:", y1);
+const y2 = 3;
+console.log("🚀 ~ y2:", y2);
+out15.textContent = y1 / y2;
+
+//Task 16//
+const out16 = document.querySelector(".out-16");
+console.log("🚀 ~ out16:", out16);
+const x1 = "Hello";
+console.log("🚀 ~ x1:", x1);
+const x2 = 5;
+console.log("🚀 ~ x2:", x2);
+out16.textContent = x1 + x2;
+
+//Task 17//
+const out17 = document.querySelector(".out-17");
+console.log("🚀 ~ out17:", out17);
+//console.dir(out17)//
+
+//Task 18//
+let out18 = document.querySelector(".out-18");
+console.log("🚀 ~ out18:", out18);
+out18 = 5;
+console.log("🚀 ~ out18:", out18);
+
+//Task 19//
+let out19 = document.querySelector(".out-19");
+console.log("🚀 ~ out19:", out19);
+out19 = document.querySelector(".out-19-test");
+console.log("🚀 ~ out19:", out19);
+
+//Task 20//
+const out20 = document.querySelector(".out-20");
+console.log("🚀 ~ out20:", out20);
+out20.textContent = "<h2>Hi</h2>";
